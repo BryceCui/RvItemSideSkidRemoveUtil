7a98a5331a22bdfe1f1f7eb30f4018837e60bcf5
cuipengyu <cuipengyusoul@gmail.com>
2018-04-16 23:00:38 +0800

    开源库的引用实现

---
+++ app/src/main/java/com/cuipengyu/rvitemsideskidremoveutil/BaseRvAdapter.java
@@ -0,0 +1,12 @@
+package com.cuipengyu.rvitemsideskidremoveutil;
+
+/**
+ * Create by    ： 崔鹏宇
+ * Time  is     ： 2018/4/16
+ * Email        ： cuipengyusoul@gmail.com
+ * Github       ： https://github.com/SolitarySoul
+ * Instructions ：
+ */
+// TODO: 2018/4/16  封装adapter 下拉刷新 上拉加载  无网络显示  多布局
+public class BaseRvAdapter {
+}
